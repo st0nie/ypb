3099fb3c1bda61bfb78a77d7ff03e2322404b4f2
Stllok <cl20103B11@yahoo.com.hk>
2025-05-02 19:48:38 +0800

    fix: wrong slice format on hash

--- src/util/handler.rs
+++ src/util/handler.rs
@@ -150,7 +150,7 @@ pub async fn put_handler(
     use base64::prelude::*;
 
     let hash_bytes = HASHER.checksum(&bytes).to_be_bytes();
-    let hash = base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(&hash_bytes[0..4]);
+    let hash = &BASE64_URL_SAFE.encode(&hash_bytes)[0..4];
 
     let file_name = format!("{}.txt", hash);
     let file_path = FilePath::new(&state.args.file_path).join(&file_name);
