595668e6b5942bae1c11b2fd78bed42060273d90
Ston Jia <ston.jia@outlook.com>
2025-11-14 11:13:51 +0800

    chore: add description

--- src/util/args.rs
+++ src/util/args.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 
+/// Yet another PasteBin.
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
 pub struct Args {
@@ -8,18 +9,18 @@ pub struct Args {
     pub port: u16,
 
     /// Path to the file storage directory
-    #[clap(short, long, default_value = "./files")]
+    #[arg(short, long, default_value = "./files")]
     pub file_path: String,
 
     /// Period to check for expired files (in seconds)
-    #[clap(short, long, default_value_t = 3600)]
+    #[arg(short, long, default_value_t = 3600)]
     pub clean_period: u64,
 
     /// File size limit (in bytes)
-    #[clap(short, long, default_value_t = 10 * 1024 * 1024)]
+    #[arg(short, long, default_value_t = 10 * 1024 * 1024)]
     pub limit_size: usize,
 
     /// Syntax highlight theme (highlight.js)
-    #[clap(short, long, default_value = "vs")]
+    #[arg(short, long, default_value = "vs")]
     pub syntax_theme: String,
 }
